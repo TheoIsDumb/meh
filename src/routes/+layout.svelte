50a229f470b345c975626c7c999ba00c7c26c244
theo <lesgooo1993@proton.me>
2023-09-03 05:04:54 +0530

    added opengraph image + data

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -7,4 +7,23 @@
 <MetaTags
     title="meh."
     description="An endless loop of meh."
+    openGraph={{
+    url: 'https://meh-two.vercel.app',
+    title: 'meh.',
+    description: "An endless loop of meh.",
+    images: [
+      {
+        url: 'https://meh-two.vercel.app/og.jpg',
+        width: 1200,
+        height: 630,
+        alt: "meh - An endless loop of meh."
+      },
+    ],
+  }}
+  twitter={{
+    cardType: 'summary_large_image',
+    title: 'meh.',
+    description: "An endless loop of meh.",
+    image: 'https://meh-two.vercel.app/og.jpg',
+  }}
 />
