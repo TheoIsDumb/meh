632a670ae6ba90c68db7e1491c39d0dbfd0e2d9a
theo <lesgooo1993@proton.me>
2023-09-03 04:53:17 +0530

    added source code link to sidebar

--- src/lib/components/Sidebar.svelte
+++ src/lib/components/Sidebar.svelte
@@ -1,11 +1,16 @@
 <script>
   import Audio from './Audio.svelte';
   import ColorPalette from './ColorPalette.svelte';
+  import Github from '$lib/icons/Github.svelte';
 </script>
 
 <div class="sidebar">
   <ColorPalette />
   <Audio />
+
+  <a href="https://github.com/theoisdumb/meh" target="_blank" noopener noreferrer>
+    <Github />
+  </a>
 </div>
 
 <style>
@@ -16,5 +21,6 @@
     display: flex;
     flex-direction: column;
     gap: 1rem;
+    align-items: center;
   }
 </style>
