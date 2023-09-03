27eeebe66340b8f38fecc5bca526561136a52fd1
theo <lesgooo1993@proton.me>
2023-09-03 18:11:00 +0530

    added orbitcontrols

--- src/lib/components/Scene.svelte
+++ src/lib/components/Scene.svelte
@@ -1,5 +1,6 @@
 <script>
   import { T, useFrame } from '@threlte/core'
+  import { OrbitControls } from '@threlte/extras'
   import { color } from '$lib/store'
 
   let rotation = 0
@@ -14,7 +15,15 @@
   fov={100}
   near={0.1}
   far={1000}
-/>
+>
+  <OrbitControls 
+    enableDamping
+    dampingFactor={0.05}
+    minDistance={0.1}
+    maxDistance={20}
+    autoRotate={true}
+  />
+</T.PerspectiveCamera>
 
 <T.DirectionalLight position={[0, 10, 10]} intensity={0.5} />
 <T.AmbientLight color="#ffffff" intensity={0.2} />
